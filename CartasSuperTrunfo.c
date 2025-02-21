d4e374fc4717ec4009583a50396279b763cb1e84
jehprogramacaogit <jefersonsilvajr096@gmail.com>
2025-02-21 18:42:31 +0000

    comecar denovo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-int main(){
-    printf("bem vindos ao jogo de cartas super trunfo!\n");
-
-    char estado = "Amazônia";
-    char estado = "Bahia";
-    char código[20] = "A01";
-    char código[20] = "B01";
-    char nomedacidade[30] = "Manaus";
-
-
-    
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
+    int main(){
+        
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
