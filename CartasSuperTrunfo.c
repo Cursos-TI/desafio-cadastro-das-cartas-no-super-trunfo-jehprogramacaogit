771117248698e901c672b6f1191733683b7476ec
jehprogramacaogit <jefersonsilvajr096@gmail.com>
2025-02-21 22:06:58 +0000

    estrutura carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+    //Estrutura para armazenar os dados da cidade
+    char estado = 'A';
+    char código[4] = "A01";
+    char nomedacidade[20] = "manaus";
+    int polulação = 4281.000;
+    float área = 1571.000;
+    float Pib = 429.000;
+    int númerodepontosturísticos = 60;
+
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     int main(){
-        
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
